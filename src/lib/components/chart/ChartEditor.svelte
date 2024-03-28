dd099ca5a581ba4df677fd06ccee9041505cb2e5
Herover <hi+git@leonora.app>
2024-03-28 21:17:56 +0100

    Add input label tags

--- src/lib/components/chart/ChartEditor.svelte
+++ src/lib/components/chart/ChartEditor.svelte
@@ -8,60 +8,76 @@
 </script>
 
 <p>
-  Title: <input
-    value={spec.chart.title}
-    on:keyup={(e) => chartScope.setConfigTitle(e.currentTarget.value)}
-  />
+  <label>
+    Title: <input
+      value={spec.chart.title}
+      on:keyup={(e) => chartScope.setConfigTitle(e.currentTarget.value)}
+    />
+  </label>
 </p>
 <p>
-  Sub title: <input
-    value={spec.chart.subTitle}
-    on:keyup={(e) => chartScope.setConfigSubTitle(e.currentTarget.value)}
-  />
+  <label>
+    Sub title: <input
+      value={spec.chart.subTitle}
+      on:keyup={(e) => chartScope.setConfigSubTitle(e.currentTarget.value)}
+    />
+  </label>
 </p>
 <p>
-  Source text (left): <input
-    value={spec.chart.sourceTextLeft}
-    on:keyup={(e) => chartScope.setSourceTextLeft(e.currentTarget.value)}
-  />
+  <label>
+    Source text (left): <input
+      value={spec.chart.sourceTextLeft}
+      on:keyup={(e) => chartScope.setSourceTextLeft(e.currentTarget.value)}
+    />
+  </label>
 </p>
 <p>
-  Source link (left): <input
-    value={spec.chart.sourceTextLeftLink}
-    on:keyup={(e) => chartScope.setSourceTextLeftLink(e.currentTarget.value)}
-  />
+  <label>
+    Source link (left): <input
+      value={spec.chart.sourceTextLeftLink}
+      on:keyup={(e) => chartScope.setSourceTextLeftLink(e.currentTarget.value)}
+    />
+  </label>
 </p>
 <p>
-  Source text (right): <input
-    value={spec.chart.sourceTextRight}
-    on:keyup={(e) => chartScope.setSourceTextRight(e.currentTarget.value)}
-  />
+  <label>
+    Source text (right): <input
+      value={spec.chart.sourceTextRight}
+      on:keyup={(e) => chartScope.setSourceTextRight(e.currentTarget.value)}
+    />
+  </label>
 </p>
 <p>
-  Source link (right): <input
-    value={spec.chart.sourceTextRightLink}
-    on:keyup={(e) => chartScope.setSourceTextLeftRight(e.currentTarget.value)}
-  />
+  <label>
+    Source link (right): <input
+      value={spec.chart.sourceTextRightLink}
+      on:keyup={(e) => chartScope.setSourceTextLeftRight(e.currentTarget.value)}
+    />
+  </label>
 </p>
 <p>
-  Height: <input
-    value={spec.chart.height}
-    on:keyup={(e) =>
-      chartScope.setConfigHeight(Number.parseInt(e.currentTarget.value))}
-    on:change={(e) =>
-      chartScope.setConfigHeight(Number.parseInt(e.currentTarget.value))}
-    type="number"
-  />
+  <label>
+    Height: <input
+      value={spec.chart.height}
+      on:keyup={(e) =>
+        chartScope.setConfigHeight(Number.parseInt(e.currentTarget.value))}
+      on:change={(e) =>
+        chartScope.setConfigHeight(Number.parseInt(e.currentTarget.value))}
+      type="number"
+    />
+  </label>
 </p>
 <p>
-  Width: <input
-    value={spec.chart.width}
-    on:keyup={(e) =>
-      chartScope.setConfigWidth(Number.parseInt(e.currentTarget.value))}
-    on:change={(e) =>
-      chartScope.setConfigWidth(Number.parseInt(e.currentTarget.value))}
-    type="number"
-  />
+  <label>
+    Width: <input
+      value={spec.chart.width}
+      on:keyup={(e) =>
+        chartScope.setConfigWidth(Number.parseInt(e.currentTarget.value))}
+      on:change={(e) =>
+        chartScope.setConfigWidth(Number.parseInt(e.currentTarget.value))}
+      type="number"
+    />
+  </label>
 </p>
 
 {#if spec.chart.chartType == "hBar"}
--- src/lib/components/chart/HBarEditor.svelte
+++ src/lib/components/chart/HBarEditor.svelte
@@ -13,80 +13,95 @@
 </script>
 
 <p>
-  Label width: <input
-    value={spec.chart.hBar.labelWidth}
-    on:keyup={(e) =>
-      dbHBar.setLabelWidth(Number.parseInt(e.currentTarget.value))}
-    on:change={(e) =>
-      dbHBar.setLabelWidth(Number.parseInt(e.currentTarget.value))}
-    type="number"
-  />
+  <label>
+    Label width: <input
+      value={spec.chart.hBar.labelWidth}
+      on:keyup={(e) =>
+        dbHBar.setLabelWidth(Number.parseInt(e.currentTarget.value))}
+      on:change={(e) =>
+        dbHBar.setLabelWidth(Number.parseInt(e.currentTarget.value))}
+      type="number"
+    />
+  </label>
 </p>
 <p>
-  Categories from:
-  <select
-    value={spec.chart.hBar.categories}
-    on:change={(e) => dbHBar.setCategories(e.currentTarget.value)}
-  >
-    <option>{""}</option>
-    {#each spec.data.rows as row}
-      <option>{row.key}</option>
-    {/each}
-  </select>
+  <label>
+    Categories from:
+    <select
+      value={spec.chart.hBar.categories}
+      on:change={(e) => dbHBar.setCategories(e.currentTarget.value)}
+    >
+      <option>{""}</option>
+      {#each spec.data.rows as row}
+        <option>{row.key}</option>
+      {/each}
+    </select>
+  </label>
 </p>
 <p>
-  Sub categories from:
-  <select
-    value={spec.chart.hBar.subCategories}
-    on:change={(e) => dbHBar.setSubCategories(e.currentTarget.value)}
-  >
-    <option>{""}</option>
-    {#each spec.data.rows as row}
-      <option>{row.key}</option>
-    {/each}
-  </select>
+  <label>
+    Sub categories from:
+    <select
+      value={spec.chart.hBar.subCategories}
+      on:change={(e) => dbHBar.setSubCategories(e.currentTarget.value)}
+    >
+      <option>{""}</option>
+      {#each spec.data.rows as row}
+        <option>{row.key}</option>
+      {/each}
+    </select>
+  </label>
 </p>
 <p>
-  Values from:
-  <select
-    value={spec.chart.hBar.value}
-    on:change={(e) => dbHBar.setValue(e.currentTarget.value)}
-  >
-    {#each spec.data.rows.filter((r) => r.type == "number") as row}
-      <option>{row.key}</option>
-    {/each}
-  </select>
+  <label>
+    Values from:
+    <select
+      value={spec.chart.hBar.value}
+      on:change={(e) => dbHBar.setValue(e.currentTarget.value)}
+    >
+      {#each spec.data.rows.filter((r) => r.type == "number") as row}
+        <option>{row.key}</option>
+      {/each}
+    </select>
+  </label>
 </p>
 <p>
-  Scale from:
-  <input
-    value={scale.dataRange[0]}
-    on:keyup={(e) =>
-      chart.setScaleFrom(scaleIndex, Number.parseInt(e.currentTarget.value))}
-    on:change={(e) =>
-      chart.setScaleFrom(scaleIndex, Number.parseInt(e.currentTarget.value))}
-    type="number"
-    style="width: 90px"
-  />
-  <input
-    value={scale.dataRange[1]}
-    on:keyup={(e) =>
-      chart.setScaleTo(scaleIndex, Number.parseInt(e.currentTarget.value))}
-    on:change={(e) =>
-      chart.setScaleTo(scaleIndex, Number.parseInt(e.currentTarget.value))}
-    type="number"
-    style="width: 90px"
-  />
+  <label
+    >Scale from:
+    <input
+      value={scale.dataRange[0]}
+      on:keyup={(e) =>
+        chart.setScaleFrom(scaleIndex, Number.parseInt(e.currentTarget.value))}
+      on:change={(e) =>
+        chart.setScaleFrom(scaleIndex, Number.parseInt(e.currentTarget.value))}
+      type="number"
+      style="width: 90px"
+    />
+  </label>
+  <label>
+    to
+    <input
+      value={scale.dataRange[1]}
+      on:keyup={(e) =>
+        chart.setScaleTo(scaleIndex, Number.parseInt(e.currentTarget.value))}
+      on:change={(e) =>
+        chart.setScaleTo(scaleIndex, Number.parseInt(e.currentTarget.value))}
+      type="number"
+      style="width: 90px"
+    />
+  </label>
 </p>
 <p>
-  Repeat for each:
-  <select
-    value={spec.chart.hBar.repeat}
-    on:change={(e) => dbHBar.setRepeat(e.currentTarget.value)}
-  >
-    <option>{""}</option>
-    {#each spec.data.rows as row}
-      <option>{row.key}</option>
-    {/each}
-  </select>
+  <label
+    >Repeat for each:
+    <select
+      value={spec.chart.hBar.repeat}
+      on:change={(e) => dbHBar.setRepeat(e.currentTarget.value)}
+    >
+      <option>{""}</option>
+      {#each spec.data.rows as row}
+        <option>{row.key}</option>
+      {/each}
+    </select>
+  </label>
 </p>
