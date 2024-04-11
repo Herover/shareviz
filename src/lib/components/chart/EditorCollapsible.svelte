093c75dec2b97ec027e433d6a616b858578d77cd
Herover <hi+git@leonora.app>
2024-04-11 22:50:04 +0200

    Add way to customize editor collapsible

--- src/lib/components/chart/EditorCollapsible.svelte
+++ src/lib/components/chart/EditorCollapsible.svelte
@@ -9,19 +9,22 @@
 </script>
 
 <div class="container">
-  <button
-    class:header-open={open}
-    on:click={() => (open = !open)}
-    class="header"
-  >
-    <!-- h3 is technically not allowed inside a button, but appears like the most accessible solution if we want a
-      clickable header -->
-    {#if lvl == 1}
-      <h3>{open ? "-" : "+"} {label}</h3>
-    {:else if lvl == 2}
-      <h4>{open ? "-" : "+"} {label}</h4>
-    {/if}
-  </button>
+  <div class="header-content">
+    <button
+      class:header-open={open}
+      on:click={() => (open = !open)}
+      class="header"
+    >
+      <!-- h3 is technically not allowed inside a button, but appears like the most accessible solution if we want a
+        clickable header -->
+      {#if lvl == 1}
+        <h3>{open ? "-" : "+"} {label}</h3>
+      {:else if lvl == 2}
+        <h4>{open ? "-" : "+"} {label}</h4>
+      {/if}
+    </button>
+    <slot name="actions" />
+  </div>
   <div class="content">
     {#if open}
       <slot />
@@ -33,11 +36,18 @@
   .container {
     width: 100%;
   }
-  .header {
+  .header-content button {
     border: none;
     background-color: transparent;
     text-align: start;
-    width: 100%;
+    cursor: pointer;
+  }
+  .header {
+    flex: 1 1 auto;
+  }
+  .header-content {
+    display: flex;
+    flex-direction: row;
     border-bottom: 1px solid black;
   }
   .content {
