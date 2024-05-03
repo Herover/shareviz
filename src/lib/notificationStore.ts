0ce59d79e36f41bfb0154d6287ae6fd97c9225a8
Herover <hi+git@leonora.app>
2024-05-03 21:19:48 +0200

    Add notification store and popups

---
+++ src/lib/notificationStore.ts
@@ -0,0 +1,21 @@
+import { writable } from 'svelte/store';
+
+export enum NotificationLevel {
+  ERROR = "error",
+};
+
+export const notifications = (() => {
+  const { subscribe, set, update } = writable<{ message: string, type: NotificationLevel }[]>([]);
+
+  return {
+    subscribe,
+    addError: (message: string) => update(notifications => {
+      notifications.push({ message, type: NotificationLevel.ERROR });
+      return notifications;
+    }),
+    read: (i: number) => update(notifications => {
+      notifications.splice(i, 1);
+      return notifications;
+    }),
+  };
+})();
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { NotificationLevel, notifications } from "$lib/notificationStore";
+</script>
+
+<div class="notifications">
+  {#each $notifications as notification, i}
+    <div
+      on:click={() => notifications.read(i)}
+      on:keydown={() => notifications.read(i)}
+      class:error={notification.type == NotificationLevel.ERROR}
+      role="button"
+      class="notification"
+      tabindex="0"
+    >
+      Error: {notification.message}
+    </div>
+  {/each}
+</div>
+
+<slot />
+
+<style>
+  .notifications {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+  }
+  .notification {
+    max-width: 400px;
+    margin-top: 40px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 20px;
+    border-radius: 20px;
+    cursor: pointer;
+  }
+  .error {
+    background-color: #ff6666;
+  }
+</style>
