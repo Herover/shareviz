77df9bbf336eae345f3042d6705e4ba3922bd6d9
Herover <hi+git@leonora.app>
2024-06-22 18:48:27 +0200

    Fix problem breaking prod server

--- server_lib/sharedb.js
+++ server_lib/sharedb.js
@@ -1,7 +1,7 @@
 import ShareDB from 'sharedb';
 import json1 from 'ot-json1';
 
-import { JSONDB } from "./jsondb";
+import { JSONDB } from "./jsondb.js";
 
 ShareDB.types.register(json1.type);
 export const backend = new ShareDB({ presence: true, db: new JSONDB({}) });
