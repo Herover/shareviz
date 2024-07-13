ffe9f405c798ff5623e1299ef1f383b3706ce288
Herover <hi+git@leonora.app>
2024-07-13 22:56:38 +0200

    Place label line depending on relative position

--- src/lib/components/chart/Line/FloatingLabels.svelte
+++ src/lib/components/chart/Line/FloatingLabels.svelte
@@ -9,6 +9,7 @@
   export let yScale:
     | ScaleLinear<number, number, never>
     | ScaleTime<number, number, never>;
+  const boxes: { width: number; height: number }[] = [];
 </script>
 
 {#each lines as line}
@@ -17,19 +18,46 @@
       x={line.label.rx}
       y={line.label.ry - 8}
       fill={line.color}
+      bind:contentRect={boxes[i]}
       paint-order="stroke"
       stroke="#ffffff"
       stroke-width="3"
       text-anchor="middle">{line.label.text}</text
     >
     {#if line.label.line == LabelStyleLine.Line}
-      <line
-        x2={line.label.rx}
-        y2={line.label.ry}
-        stroke="#000000"
-        stroke-width={2}
-        stroke-linecap="round"
-      />
+      {#if line.label.ry < 0}
+        <line
+          x2={line.label.rx}
+          y2={line.label.ry}
+          stroke="#000000"
+          stroke-width={2}
+          stroke-linecap="round"
+        />
+      {:else if line.label.ry > 24}
+        <line
+          x2={line.label.rx}
+          y2={line.label.ry - 24}
+          stroke="#000000"
+          stroke-width={2}
+          stroke-linecap="round"
+        />
+      {:else if line.label.rx < 0}
+        <line
+          x2={line.label.rx + boxes[i]?.width / 2 + 8}
+          y2={line.label.ry - boxes[i]?.height / 2}
+          stroke="#000000"
+          stroke-width={2}
+          stroke-linecap="round"
+        />
+      {:else if line.label.rx > 0}
+        <line
+          x2={line.label.rx - boxes[i]?.width / 2 - 8}
+          y2={line.label.ry - boxes[i]?.height / 2}
+          stroke="#000000"
+          stroke-width={2}
+          stroke-linecap="round"
+        />
+      {/if}
     {/if}
   </g>
 {/each}
