5029ac71a254fae6817577b5c465aa8a50d5cf8f
Herover <hi+git@leonora.app>
2024-09-13 22:01:08 +0200

    Include inner label spacing for both sides on display calc

--- src/lib/components/chart/HBar/HBarRect.svelte
+++ src/lib/components/chart/HBar/HBarRect.svelte
@@ -12,17 +12,17 @@
   export let label: string;
   export let value: number;
   export let axis: Axis;
-  export let conf: HBar
+  export let conf: HBar;
 
   let textBox: DOMRect | undefined;
-  $: offset = (textBox ? barHeight - textBox.height : 0);
-  $: display = textBox ? textBox.width + offset < width : true;
+  $: offset = textBox ? barHeight - textBox.height : 0;
+  $: display = textBox ? textBox.width + offset * 2 < width : true;
 
   let textColor = "#000000";
   $: {
     try {
       // TODO: Examine if other difference functions work better here, ex. .distance, .contrast
-      // or .contrastAPCA 
+      // or .contrastAPCA
       textColor =
         chroma.deltaE(fill, "#000000") > chroma.deltaE(fill, "#ffffff")
           ? "#000000"
