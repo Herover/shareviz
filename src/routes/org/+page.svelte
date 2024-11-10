91f0ffc9a49209bc61815ef560b82db76df4456c
Herover <hi+git@leonora.app>
2024-11-10 22:43:08 +0100

    Display message if user isn't in a organization

--- src/routes/org/+page.svelte
+++ src/routes/org/+page.svelte
@@ -4,10 +4,14 @@
 
 <h1>Select organization</h1>
 
-{#each $page.data.organizations as organization}
-  <p>
-    <a href="/org/{organization.organizations.id}/team/">
-      {organization.organizations.name}
-    </a>
-  </p>
-{/each}
+{#if $page.data.organizations.length == 0}
+  <p>You are not yet a part of a organization yet.</p>
+{:else}
+  {#each $page.data.organizations as organization}
+    <p>
+      <a href="/org/{organization.organizations.id}/team">
+        {organization.organizations.name}
+      </a>
+    </p>
+  {/each}
+{/if}
