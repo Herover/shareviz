dee383093ca397777a37f364bce38b6b41988b2f
Herover <hi+git@leonora.app>
2024-12-06 22:06:30 +0100

    Add basic prettier config with svelte plugin

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+export default {
+  printWidth: 100,
+  plugins: ["prettier-plugin-svelte"],
+  overrides: [
+          {
+                  files: "*.svelte",
+                  options: {
+                          parser: "svelte"
+                  }
+          }
+  ]
+}
