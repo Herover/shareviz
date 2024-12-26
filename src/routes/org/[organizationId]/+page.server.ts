156592e565c224a17eada9d7a4a653c58791c051
Herover <hi+git@leonora.app>
2024-12-26 21:24:21 +0100

    Fix type error

--- src/routes/org/[organizationId]/+page.server.ts
+++ src/routes/org/[organizationId]/+page.server.ts
@@ -1,8 +1,7 @@
-import type { PageData } from "./$types.js";
 import { db } from "../../../../server_lib/sqlite.js";
 
 export async function load({ params, parent }) {
-  const data: PageData = await parent();
+  const data = await parent();
   const invites = data.userIsOrgAdmin ? await db.getOrganizationInvites(params.organizationId) : [];
 
   return { invites };
