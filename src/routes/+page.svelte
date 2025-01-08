b1a42b95a22cbf57d82bed46917f44927027ec8f
Herover <hi+git@leonora.app>
2025-01-08 22:28:34 +0100

    Basic signin page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { SignIn, SignOut } from "@auth/sveltekit/components";
+  import { page } from "$app/state";
+  import { redirect } from "@sveltejs/kit";
+
+  if (page.data.session?.user) {
+    redirect(302, "/org");
+  }
+</script>
+
+<main>
+  <div class="content">
+    {#if !page.data.session?.user}
+      <h1>Sign in using</h1>
+      <SignIn provider="github" signInPage="signin" />
+    {:else}
+      <p>Redirecting you to <a href="/org">organization page</a>...</p>
+    {/if}
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
